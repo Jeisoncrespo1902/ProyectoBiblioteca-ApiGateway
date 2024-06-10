2a58fc23c304cfc46d64e371604a4eebf718fc99
jrdeavila <jrdeavila@unicesar.edu.co>
2024-06-10 11:17:49 -0500

    chore: Add initial project files and configurations

---
+++ src/main/java/com/proyectobiblioteca/proyectobiblioteca/ProyectobibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectobiblioteca.proyectobiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectobibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectobibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectobiblioteca/proyectobiblioteca/ProyectobibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectobiblioteca.proyectobiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectobibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
